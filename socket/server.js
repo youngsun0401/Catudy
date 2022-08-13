a5926d963635e718400bb3ff19557971240c2328
dbckd999 <dbckd999@naver.com>
2022-08-13 21:25:45 +0900

    M:M 화상채팅 기능구현

--- socket/server.js
+++ socket/server.js
@@ -1,29 +1,32 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const { readFileSync } = require("fs");
+const { createServer } = require("https");
+const { Server } = require("socket.io");
+const express = require("express");
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const app = express();
+
+const httpServer = createServer({
+  key: readFileSync("key.pem"),
+  cert: readFileSync("cert.pem")
+}, app);
+
+const io = new Server(httpServer, { /* options */ });
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+    res.send('hello world');
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId, userId);
+        // socket.to(roomId).broadcast.emit('user-connected', userId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        socket.on('disconnect', () => {
+            // socket.to(roomId).broadcast.emit('user-disconnected', userId)
+            socket.broadcast.to(roomId).emit('user-disconnected', userId)
+        })
     })
-  })
 })
 
-server.listen(3000)
+httpServer.listen(3000);
--- src/main/resources/static/js/connectPeers.js
+++ src/main/resources/static/js/connectPeers.js
@@ -1,58 +1,69 @@
 const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
 
-const socket = io('/')
+const socket = io('https://192.168.0.36:3000', {
+    transports: ['websocket']
+})
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+    host: '/',
+    port: 3001,
+    secure: false
 })
 
+var userId;
+const roo_name = document.getElementById('room_name').value;
+document.getElementById('room_name_btn').addEventListener('click', () => {
+    // const user_name = document.getElementById('name').value;
+    console.log(roo_name, userId);
+})
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
 myVideo.muted = true
 const peers = {}
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  addVideoStream(myVideo, stream)
-
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
-  })
-
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
-})
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+    if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
+    userId = id;
+    socket.emit('join-room', roo_name, userId)
 })
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
 
-  peers[userId] = call
+    peers[userId] = call
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
 }
