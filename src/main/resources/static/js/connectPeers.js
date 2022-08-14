71946fbd975131323e8771686c2b9e1bc25179b3
dbckd999 <dbckd999@naver.com>
2022-08-14 21:06:31 +0900

    사용자 참가시 추가기능

--- src/main/resources/static/js/connectPeers.js
+++ src/main/resources/static/js/connectPeers.js
@@ -1,5 +1,11 @@
+const showings = document.getElementsByClassName('showing');
+var videoCount = 0;
+console.log(showings);
+
 const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
+myVideo.style.width = '100%';
+myVideo.style.height = '100%';
 
 const socket = io('https://192.168.0.36:3000', {
     transports: ['websocket']
@@ -14,27 +20,29 @@ var userId;
 const roo_name = document.getElementById('room_name').value;
 document.getElementById('room_name_btn').addEventListener('click', () => {
     // const user_name = document.getElementById('name').value;
-    console.log(roo_name, userId);
+    console.log('join-room', roo_name, userId);
+    socket.emit('join-room', roo_name, userId)
 })
 
+let myStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
+    myStream = stream;
     addVideoStream(myVideo, stream)
+})
 
-    myPeer.on('call', call => {
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream)
-        })
-    })
-
-    socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream)
+myPeer.on('call', call => {
+    call.answer(myStream)
+    const video = document.createElement('video')
+    video.style.width = '100%';
+    video.style.height = '100%';
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
     })
 })
+
 myVideo.muted = true
 const peers = {}
 
@@ -42,14 +50,19 @@ socket.on('user-disconnected', userId => {
     if (peers[userId]) peers[userId].close()
 })
 
+socket.on('user-connected', userId => {
+    connectToNewUser(userId, myStream)
+})
+
 myPeer.on('open', id => {
-    userId = id;
-    socket.emit('join-room', roo_name, userId)
+    userId = id
 })
 
 function connectToNewUser(userId, stream) {
     const call = myPeer.call(userId, stream)
     const video = document.createElement('video')
+    video.style.width = '100%';
+    video.style.height = '100%';
     call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream)
     })
@@ -61,9 +74,11 @@ function connectToNewUser(userId, stream) {
 }
 
 function addVideoStream(video, stream) {
+    console.log('addVideoStream', videoCount, video, stream);
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
         video.play()
     })
-    videoGrid.append(video)
+    showings[videoCount++].append(video);
+    // videoGrid.append(video)
 }
